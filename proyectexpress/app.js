4cae0e739a8cf0500cb67c7b7e9aabe6668bb2ba
aricmtz <39094598+aricmtz@users.noreply.github.com>
2022-05-11 21:56:17 -0500

    Con 2 endpoint

--- proyectexpress/app.js
+++ proyectexpress/app.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var app =express();
 
-app.get('/', function (req, res){
+app.get('/Hola', function (req, res){
 
     res.send('Hola mundo!');
 });
 
+app.get('/adios', function (req, res){
 
+    res.send('Adios!');
+});
 app.listen (3000, function(){
 
 console.log('Aplicacion ejemplo, escuchando el puerto 3000');
