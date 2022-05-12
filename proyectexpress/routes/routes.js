f0011fc156a4f4e9c004c246104371484d94a1e5
aricmtz <39094598+aricmtz@users.noreply.github.com>
2022-05-12 12:26:37 -0500

    Agregar un nuevo usuario

--- proyectexpress/routes/routes.js
+++ proyectexpress/routes/routes.js
@@ -47,5 +47,11 @@ app.get('users/:id',(request,response) =>{
 
 });
 
+app.post('/users/:id', (request, response) => {
+    pool.query('INSERT INTO users SET ?', request.body, (error, result) => {
+        if (error) throw error;
 
+        response.status(201).send(`User added with ID: ${result.insertId}`);
+    });
+});
 }
